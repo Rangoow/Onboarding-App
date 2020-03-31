a98b0c0ecf84ac01f9d599f4d94c51c3946d5ef2
Noé Esparsa <noe.esp@hotmail.fr>
2020-03-31 02:13:36 +0200

    squelette build (adapter viewpager idnicator)
    (adapter viewpager idnicator)
    reuse of 1 layout for each screen by modifying field

---
+++ app/src/main/java/com/example/onboardingapplication/IntroActivity.java
@@ -0,0 +1,132 @@
+package com.example.onboardingapplication;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    SlidePageAdapter slidePageAdapter;
+    TabLayout pageIndicator;
+    Button btnNext;
+    int position = 0;
+    private ValueAnimator valueAnimator;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //make activity full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_intro);
+
+        //hide action bar
+        getSupportActionBar().hide();
+        //ini views
+        btnNext = findViewById(R.id.button_next);
+        pageIndicator = findViewById(R.id.page_Indicator);
+
+
+        //fill list screen
+
+        final List<ScreenItem> list = new ArrayList<>();
+        list.add(new ScreenItem(getString(R.string.topic_visit_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_localization));
+        list.add(new ScreenItem(getString(R.string.topic_information_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_informations));
+        list.add(new ScreenItem(getString(R.string.topic_administration_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_administration));
+        list.add(new ScreenItem(getString(R.string.topic_network_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_network));
+        list.add(new ScreenItem(getString(R.string.topic_places_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_localization));
+        list.add(new ScreenItem(getString(R.string.topic_lunch_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_lunch));
+        list.add(new ScreenItem(getString(R.string.topic_market_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_market));
+        list.add(new ScreenItem(getString(R.string.topic_entertainment_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_entertainement));
+        list.add(new ScreenItem(getString(R.string.topic_toilets_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_toilet));
+        list.add(new ScreenItem(getString(R.string.topic_feedback_title),"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",R.drawable.ic_feedback));
+
+
+        //setup viewpager
+        screenPager = findViewById(R.id.screen_viewpager);
+        slidePageAdapter = new SlidePageAdapter(this,list);
+        screenPager.setAdapter(slidePageAdapter);
+
+
+        //setup tablayout with viewpager
+
+        pageIndicator.setupWithViewPager(screenPager);
+
+        //next button click listener
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = screenPager.getCurrentItem();
+                if (position < list.size()){
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+
+                if (position == list.size()){ //when reach last screen
+                    //nothing for the moment
+                    //IDEA
+                    //But can hide page indicator and next button to display a Start Visit button and start the visit
+                }
+            }
+        });
+
+        valueAnimator = ValueAnimator.ofObject(new ArgbEvaluator(),
+                getResources().getColor(R.color.topic1Backgroundcolor),
+                getResources().getColor(R.color.topic2Backgroundcolor),
+                getResources().getColor(R.color.topic3Backgroundcolor),
+                getResources().getColor(R.color.topic4Backgroundcolor),
+                getResources().getColor(R.color.topic5Backgroundcolor),
+                getResources().getColor(R.color.topic6Backgroundcolor),
+                getResources().getColor(R.color.topic7Backgroundcolor),
+                getResources().getColor(R.color.topic8Backgroundcolor),
+                getResources().getColor(R.color.topic9Backgroundcolor),
+                getResources().getColor(R.color.topic10Backgroundcolor));
+
+
+        valueAnimator.setDuration((10-1)*10000000000l);
+
+        screenPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                valueAnimator.setCurrentPlayTime((long)((positionOffset + position)*10000000000l));
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        valueAnimator.addUpdateListener((new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                screenPager.setBackgroundColor((Integer)animation.getAnimatedValue());
+            }
+        }));
+
+    }
+}
---
+++ app/src/main/java/com/example/onboardingapplication/SlidePageAdapter.java
@@ -0,0 +1,59 @@
+package com.example.onboardingapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class SlidePageAdapter extends PagerAdapter {
+
+    Context context;
+    List<ScreenItem> ListScreen;
+
+    public SlidePageAdapter(Context context, List<ScreenItem> listScreen) {
+        this.context = context;
+        ListScreen = listScreen;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreen = inflater.inflate(R.layout.layout_screen,null);
+        ImageView imgSlide = layoutScreen.findViewById(R.id.topic_img);
+        TextView topicTitle = layoutScreen.findViewById(R.id.topic_title);
+        TextView topicDesc = layoutScreen.findViewById(R.id.topic_description);
+
+        topicTitle.setText(ListScreen.get(position).getTitle());
+        topicDesc.setText(ListScreen.get(position).getDescription());
+        imgSlide.setImageResource(ListScreen.get(position).getScreenImg());
+
+        container.addView(layoutScreen);
+
+        return layoutScreen;
+    }
+
+    @Override
+    public int getCount() {
+        return ListScreen.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
