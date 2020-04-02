1fa30fa34a26447a684c2177307e3d28db3757ae
rtatin <rtwam@hotmail.fr>
2020-04-02 11:01:02 +0200

    Transition accueil> pages onboarding avec boutons

--- app/src/main/java/com/example/onboardingapplication/HomeActivity.java
+++ app/src/main/java/com/example/onboardingapplication/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.onboardingapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button startActivityButton = (Button) findViewById(R.id.buttonStudent);
+        startActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StudentTopicsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button startActivityButton2 = (Button) findViewById(R.id.buttonTeacher);
+        startActivityButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TeacherTopicsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
